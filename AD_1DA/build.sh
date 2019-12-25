c4a1cb0b578b7425711f373102bc920d34f03d46
nasm <40870683+n4sm@users.noreply.github.com>
2019-12-25 23:53:34 +0100

    Add files via upload

--- AD_1DA/build.sh
+++ AD_1DA/build.sh
@@ -1,1 +1,1 @@
-gcc *.c -o main -lbfd -lkeystone -lstdc++ -lm -lcapstone -g && ./regen.sh && nasm -f elf64 test.asm -o test.o && ld test.o -o test && ./test
+gcc *.c -o main -lbfd -lkeystone -lstdc++ -lm -lcapstone -g && chmod +x regen.sh && ./regen.sh && nasm -f elf64 test.asm -o test.o && ld test.o -o test && ./test
