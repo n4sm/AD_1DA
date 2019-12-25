0c9abd04cefe8c38a3a5caa09647d171e9611424
nasm <40870683+n4sm@users.noreply.github.com>
2019-12-25 17:07:38 +0100

    Add files via upload

--- AD_1DA/misc.c
+++ AD_1DA/misc.c
@@ -28,12 +28,9 @@ void help(char **argv){
     printf("Help : \n");
     printf("\t\t%s -h : Show this help\n", argv[0]);
     printf("\t\t%s <target_binary> -o <stub to inject>: Basic obfuscation (in work)\n", argv[0]);
-    printf("\t\t%s <target_binary> --add-code-only <stub_to inject>: Add only the executable bytes at the end of the pt_load (not availaible)\n", argv[0]);
     printf("\t\t%s <target_binary> -m <stub_to inject>: Create a new binary (<target_binary>.p4cked), which will be metamorphic and polymorphic\n", argv[0]);
-    printf("\t\t%s <target_binary> --add-code-only --raw-data <stub_to inject>: * (not availaible)\n", argv[0]);
     printf("\t\t%s <target_binary> -o <stub_to_inject> -pie: Inject stub_to_inject and patching it as a stub injected in a position independant executable binary\n", argv[0]);
     printf("\n");
-    printf("* The stub is automatically an elf but you can indicate the --raw-data options if you want to inject directly assembly instructions from your stub\n");
 }
 
 int exit_clean(unsigned char *text_stub){
