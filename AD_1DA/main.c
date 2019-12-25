0d3cc37e548126032b897ba555cc8abeb45ace8d
nasm <40870683+n4sm@users.noreply.github.com>
2019-12-25 03:29:05 +0100

    Add files via upload

--- AD_1DA/main.c
+++ AD_1DA/main.c
@@ -39,10 +39,32 @@ int main(int argc, char **argv){
     {
         ssize_t len_stub = 0;
         unsigned char *text_stub = init_map_and_get_stub(argv[3], &len_stub);
-        add_section_ovrwrte_ep_inject_code(argv[1], ".p4cked", text_stub, len_stub);
+        add_section_ovrwrte_ep_inject_code(argv[1], ".p4cked", text_stub, len_stub, false, false);
 
         exit_clean(text_stub);
     }
+
+    else if (argc == 5 && !strcmp(argv[2], "-o") && !strcmp(argv[4], "-pie"))
+    {
+        ssize_t len_stub = 0;
+        unsigned char *text_stub = init_map_and_get_stub(argv[3], &len_stub);
+
+        add_section_ovrwrte_ep_inject_code(argv[1], ".p4cked", text_stub, len_stub, true, false);
+
+        exit_clean(text_stub);
+    }
+    
+
+    else if (argc == 5 && !strcmp(argv[2], "-m") && !strcmp(argv[4], "-pie"))
+    {
+        ssize_t len_stub = 0;
+        unsigned char *text_stub = init_map_and_get_stub(argv[3], &len_stub);
+
+        add_section_ovrwrte_ep_inject_code(argv[1], ".p4cked", text_stub, len_stub, true, true);
+
+        exit_clean(text_stub);
+    }
+    
 
     else if (argc == 4 && !strcmp(argv[2], "--add-code-only"))
     {
--- AD_1DA/misc.c
+++ AD_1DA/misc.c
@@ -16,6 +16,7 @@
 #include "include.h"
 
 #include <capstone/capstone.h>
+#include <keystone/keystone.h> 
 
 void show_help(char **argv){
     printf("Usage : %s <target_file> <option> <stub_to_inject>\n", argv[0]);
@@ -26,9 +27,11 @@ void help(char **argv){
     printf("AD_1DA is a modern tool made in order to obfuscate your elf binaries\n");
     printf("Help : \n");
     printf("\t\t%s -h : Show this help\n", argv[0]);
-    printf("\t\t%s <target_binary> -o <stub to inject>: Basic obfuscation\n", argv[0]);
-    printf("\t\t%s <target_binary> --add-code-only <stub_to inject>: Add only the executable bytes at the end of the pt_load\n", argv[0]);
-    printf("\t\t%s <target_binary> --add-code-only --raw-data <stub_to inject>: *\n", argv[0]);
+    printf("\t\t%s <target_binary> -o <stub to inject>: Basic obfuscation (in work)\n", argv[0]);
+    printf("\t\t%s <target_binary> --add-code-only <stub_to inject>: Add only the executable bytes at the end of the pt_load (not availaible)\n", argv[0]);
+    printf("\t\t%s <target_binary> -m <stub_to inject>: Create a new binary (<target_binary>.p4cked), which will be metamorphic and polymorphic\n", argv[0]);
+    printf("\t\t%s <target_binary> --add-code-only --raw-data <stub_to inject>: * (not availaible)\n", argv[0]);
+    printf("\t\t%s <target_binary> -o <stub_to_inject> -pie: Inject stub_to_inject and patching it as a stub injected in a position independant executable binary\n", argv[0]);
     printf("\n");
     printf("* The stub is automatically an elf but you can indicate the --raw-data options if you want to inject directly assembly instructions from your stub\n");
 }
