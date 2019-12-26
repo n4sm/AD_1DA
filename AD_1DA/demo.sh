e08096aa4caaed26ad7df7de78b5b89a3a582af4
nasm <40870683+n4sm@users.noreply.github.com>
2019-12-26 15:01:05 +0100

    Add files via upload

--- AD_1DA/demo.sh
+++ AD_1DA/demo.sh
@@ -1,1 +1,13 @@
-while [ 1 ]; do md5sum ./$1 && ./$1 > /dev/null; done;
+#!/bin/zsh
+
+typeset nbr
+
+nbr=0
+
+while true
+do
+	let "nbr+=1"
+	echo "$nbr md5 : \c"
+	md5sum $1
+	./$1 > /dev/null
+done
