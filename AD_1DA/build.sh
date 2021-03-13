a40906dc007c5dd1f339063b7f865b502543e840
n4sm <hobbyste.sda@gmail.com>
2021-03-13 02:11:04 +0100

    delete things

--- AD_1DA/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc *.c -o main -lbfd -lkeystone -lstdc++ -lm -lcapstone -g && chmod +x regen.sh && chmod +x demo.sh && ./regen.sh && nasm -f elf64 test.asm -o test.o && ld test.o -o test && ./test
